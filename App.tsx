6153f1a4f3ddd969b24110e09aadef683dc1fc5b
OtavioFilipe <darksnack16@gmail.com>
2022-10-05 23:54:00 -0300

    test useState and useEffect

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { NativeBaseProvider } from "native-base";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { Dashboard } from "./src/screens/dashboard";
 
 export default function App() {
   return (
     <NativeBaseProvider>
       <View style={styles.container}>
-        <Text>Hello World</Text>
+        <Dashboard />
         <StatusBar style="auto" />
       </View>
     </NativeBaseProvider>
---
+++ src/screens/dashboard/index.tsx
@@ -0,0 +1,41 @@
+import { Button, HStack, Text, useToast } from "native-base";
+import React, { useEffect, useState } from "react";
+
+// import { Container } from './styles';
+
+export const Dashboard: React.FC = () => {
+  const [cupSize, setCupSize] = useState<number>(300);
+  const [water, setWater] = useState<number>(0);
+  const [goal, setGoal] = useState<number>(2000);
+
+  const toast = useToast();
+
+  function handleWater() {
+    setWater(water + cupSize);
+    toast.show({
+      description: `Você bebeu ${cupSize}ml de água`,
+    });
+  }
+
+  useEffect(() => {
+    if (water >= goal) {
+      toast.show({
+        description: "Você atingiu sua meta de água",
+        placement: "top",
+        colorScheme: "success",
+      });
+    }
+  }, [water]);
+
+  return (
+    <>
+      <HStack alignItems="center" justifyContent="center">
+        <Text fontSize="6xl">{water}</Text>
+        <Text fontSize="xl"> /{goal} ml</Text>
+      </HStack>
+      <Button colorScheme="primary" onPress={handleWater}>
+        Beber água
+      </Button>
+    </>
+  );
+};
